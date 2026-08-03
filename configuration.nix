f57e4ae1a69b9b10b035713b339c40a2db4266fb
aaalexliu <4alexliu@gmail.com>
2026-08-03 19:44:49 -0400

    chore: remove zoom cask

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,6 @@
       "keycastr"              # on-screen keystrokes
       "shortcat"              # keyboard-driven UI navigation
       "session-manager-plugin"  # aws ssm session manager
-      "zoom"
       "slack"
     ];
   };
